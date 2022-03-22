be26f1c4a1e16a5b56a8ba4d6c3ab9a44fa9aa42
c0b4lt-student <student.corentin.turgis@gmail.com>
2022-03-22 13:53:29 +0100

    Html finished

--- index.php
+++ index.php
@@ -9,28 +9,47 @@
           crossorigin="anonymous">
     <link rel="stylesheet" href="styles/style.css" />
 </head>
-<body>
-  <main class="vh-100 p-3 d-flex">
+<body class="container-fluid">
+  <main class="vh-100 p-3 row">
     <div class="P-board" id="P1-board">
-      <div class="P-name-container w-50 m-auto position-relative">
-        <p class="P-name P-name-active text-center m-0">PLAYER 1&nbsp&nbsp&nbsp</p>
-        <p class="text-center total-score text-danger">66</p>
+      <div class="P-name-container w-75 m-auto position-relative">
+        <p class="P-name P-name-active text-center m-0" id="P1-name">PLAYER 1&nbsp&nbsp&nbsp</p>
+        <p class="text-center total-score text-danger">0</p>
       </div>
       <div class="current-container position-relative bg-danger w-25 text-center m-auto">
         <p class="current-text m-0">CURRENT</p>
-        <p class="current-score">9</p>
+        <p class="current-score">0</p>
       </div>
     </div>
+      <div class="game-buttons position-fixed">
+        <a href="#" class="new-game-container d-flex position-relative text-reset text-decoration-none" id="new-game-button">
+          <img class="icon-button" src="images/plus_button.png" alt="button to start a new game" />
+          <p class="next-icon">&nbsp&nbsp&nbspNEW GAME</p>
+        </a>
+        <img class="dice shadow" src="images/dice/dice_1.png" alt="Picture of a dice" />
+        <div class="holdnroll-buttons position-relative">
+          <a href="#" class="roll-container d-flex text-reset text-decoration-none" id="roll-button">
+            <img class="icon-button" src="images/curved_arrow.png" alt="button to roll dice" />
+            <p class="next-icon">&nbsp&nbsp&nbspROLL DICE</p>
+          </a>
+          <a href="#" class="hold-container d-flex position-relative text-reset text-decoration-none">
+            <img class="icon-button" src="images/hold.png" alt="button to hold score" />
+            <p class="next-icon">&nbsp&nbsp&nbspHOLD</p>
+          </a>
+        </div>
+      </div>
     <div class="P-board" id="P2-board">
-      <div class="P-name-container w-50 m-auto position-relative">
-        <p class="text-center font-weight-lighter P-name P-name-unactive m-0">PLAYER 2&nbsp&nbsp&nbsp</p>
-        <p class="text-center total-score text-danger">99</p>
+      <div class="P-name-container w-75 m-auto position-relative">
+        <p class="text-center font-weight-lighter P-name P-name-unactive m-0" id="P2-name">PLAYER 2&nbsp&nbsp&nbsp</p>
+        <p class="text-center total-score text-danger">0</p>
       </div>
       <div class="current-container position-relative bg-danger w-25 text-center m-auto">
         <p class="current-text m-0">CURRENT</p>
-        <p class="current-score">6</p>
+        <p class="current-score">0</p>
       </div>
     </div>
   </main>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="script/main.js"></script>
 </body>
 </html>
